7969d29e4baeeae690ebceb6ffefea076d8ae0a6
unknown <henry975@yandex.ru>
2021-11-13 00:15:04 +0300

    2nd

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import turtle #Successfully installed PyYAML-6.0 turtle-0.0.1
-import math
+#import math
 import random
 wn = turtle.Screen()
 wn.bgcolor('black')
 
+
+
+list16 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+def randColor():
+    rand_color_list = ['#']
+    for i in range(6):
+        rand_color_list.append(random.choice(list16))
+    colour = ''.join(rand_color_list)
+    return colour
+
+
+def drawCircle(t, size):
+    for i in range(10):
+        t.circle(size)
+        size = size - size_differ_num
+        print("size ", size)
+
+def drawSpecial(t, size, repeat):
+    for i in range(repeat):
+        drawCircle(t, size)
+        t.right(360/repeat)
+
+
 size_differ_num = 1
-color_list = ['white', 'yellow', 'blue', 'orange', 'pink', 'green', 'red', 'purple', 'black', '#6DF405', '#5CCEC8', '#BBF876', '#ADE7E5', '#F20765', '#9B012C', '#EB08EF', '#D8B206', '#9D87B5']
-color_list_num = 0
 size_num = 10
 
 while(True):
     Model = turtle.Turtle()
     Model.speed(0)
-    Model.color(random.choice(color_list))#?
+    Model.color(randColor())
     rotate = int(360)
-    def drawCircle(t, size):
-        for i in range(10):
-            t.circle(size)
-            size = size - size_differ_num
-            print("size ", size)
-    def drawSpecial(t, size, repeat):
-        for i in range(repeat):
-            drawCircle(t, size)
-            t.right(360/repeat)
     drawSpecial(Model,size_num,10)
-    size_differ_num+=1
-    color_list_num+=1
-    print(size_num)
+    size_differ_num += 1
     size_num += 10
+
 
 
 
